010b9fa6110e6248d3ce7385748729d9d2005882
Daniel Cary <danieljmcary@gmail.com>
2023-05-08 20:10:30 -0400

    added export to json and a sort toggle, plus updated formatting

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { Container, Button, ListGroup, Row, Col } from 'react-bootstrap';
 import Stack from 'react-bootstrap/Stack';
-import Badge from 'react-bootstrap/Badge';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Card from 'react-bootstrap/Card';
+import * as Icon from 'react-bootstrap-icons';
+import 'bootswatch/dist/darkly/bootstrap.min.css';
 
+// need favicon
 class RGBAColor {
     static readonly WHITE = new RGBAColor(0, 0, 0, 255);
     r: number = 0;
@@ -52,6 +56,8 @@ type AppState = {
     currentImgName?: string,
     currentSample: PixelSample,
     pickedSamples: PixelSample[],
+    dragging: boolean;
+    sortToggle: boolean;
 };
 
 type PixelSampleComponentProps = {
@@ -62,22 +68,32 @@ class PixelSampleComponent extends React.Component<PixelSampleComponentProps, an
 {
 
     render(): React.ReactNode {
-        return <Stack direction='horizontal' gap={3}>
-            <p>X={this.props.sample.pos.x}, Y={this.props.sample.pos.y}</p>
+        return <Stack direction='horizontal' gap={2}>
+            <p style={{ width: '3rem' }}>
+                X={this.props.sample.pos.x}
+                <br />
+                Y={this.props.sample.pos.y}
+            </p>
             <div className="vr" />
-            <svg width={24} height={24}>
-                <rect width={24} height={24}
+            <svg width={30} height={30}>
+                <rect width={30} height={30}
                     fill={this.props.sample.color.hexStr()}
                     stroke='black'
                     strokeWidth={2}
                 />
             </svg>
-            <p>{this.props.sample.color.hexStr()}</p>
-            <p>{this.props.sample.color.rgbaStr()}</p>
+            <p>
+                {this.props.sample.color.hexStr()}
+                <br />
+                {this.props.sample.color.rgbaStr()}
+            </p>
         </Stack>
     }
 
 }
+
+// supported mime types
+const imageTypes = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
 
 class App extends React.Component<any, AppState>  {
 
@@ -87,16 +103,26 @@ class App extends React.Component<any, AppState>  {
         this.state = {
             currentSample: new PixelSample(0, 0, RGBAColor.WHITE),
             pickedSamples: [],
+            dragging: false,
+            sortToggle: false,
         };
+    }
 
-        // TODO: load default image
-        // TODO?: change mouse/cursor when dragging
-        // TODO?: implement scale/zoom
+    componentDidMount(): void {
+        let canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
 
+        canvas.style.cursor = 'crosshair';
+
+        let ctx = canvas.getContext('2d')!;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.font = "22px Segoe UI";
+        ctx.fillStyle = 'black';
+        ctx.fillText('Drop or Upload Image', 10, canvas.height / 2, canvas.width - 20);
     }
 
     async setImage(file: File): Promise<void> {
-        const imageTypes = ["image/png", "image/jpeg", "image/jpg"];
         if (!imageTypes.includes(file.type)) {
             // unsupported file mime type
             console.error("Bad or unsupported file type: %s", file.type);
@@ -114,124 +140,177 @@ class App extends React.Component<any, AppState>  {
             this.state.currentImg!.close();
         }
         this.setState({ currentImg: img, currentImgName: file.name }, () => {
-            // TODO: ensure image dim's do not go past canvas's max
             canvas.width = this.state.currentImg!.width;
             canvas.height = this.state.currentImg!.height;
             canvas.getContext("2d")?.drawImage(this.state.currentImg!, 0, 0);
+            document.title = 'Pixel Picker - ' + file.name;
             return Promise.resolve();
         });
     }
 
+    exportPointsToJSON() {
+        let data = JSON.stringify(this.state.pickedSamples);
+        let blob = new Blob([data], { type: 'text/json;charset=utf-8' })
+
+        // Save file... don't use new, experimental API 'showSaveFilePicker'
+        // instead, create a link HTML element and 'click' it to download
+        let blobUrl = URL.createObjectURL(blob);
+        const anchor = document.createElement('a');
+        anchor.href = blobUrl;
+        anchor.target = "_blank";
+        anchor.download = "points.json";
+
+        // Trigger the file download
+        anchor.click();
+
+        // Release the blobUrl that was created
+        URL.revokeObjectURL(blobUrl);
+    }
+
     render(): React.ReactNode {
         return <div>
-            <input
-                id="myInputFile"
-                type="file"
-                accept="image/jpeg, image/png, image/jpg"
-                style={{ display: "none" }}
-                onChange={async (e) => {
-                    if (e.target.files!.length > 0) {
-                        let file = e.target.files!.item(0)!;
-                        await this.setImage(file);
-                    }
-                }} />
+            <Navbar bg="primary">
+                <Container fluid>
+                    <div className="font-monospace">
+                        <h3>Pixel Picker</h3>
+                        <h5 className="text-muted">v0.1</h5>
+                    </div>
+                    <Navbar.Toggle />
+                    <Button className='btn-secondary'
+                        onClick={() => { document.getElementById("myInputFile")!.click(); }}>
+                        Upload Image
+                    </Button>
+                </Container>
+            </Navbar>
             <Container fluid>
+                <br />
                 <Row>
-                    <Col>
-                        <h1>Pixel Picker</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col sm={6}>
-                        <PixelSampleComponent sample={this.state.currentSample} />
-                    </Col>
-                    <Col sm={2}>
-                        <Button onClick={() => {
-                            document.getElementById("myInputFile")!.click();
-                        }}>Upload Image</Button>
-                    </Col>
-                    <Col sm={4}>
-                        <h3>{this.state.currentImgName}</h3>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <canvas id="myCanvas" width="200" height="100"
-                            onDrop={async (e) => {
-                                // prevents file from being opened in browser
-                                e.stopPropagation();
-                                e.preventDefault();
-
-                                if (e.dataTransfer.files.length > 0) {
-                                    let firstFile = e.dataTransfer.files.item(0)!;
-                                    if (firstFile != null) {
-                                        await this.setImage(firstFile);
-                                    }
+                    <Col md={8}>
+                        <input
+                            id="myInputFile"
+                            type="file"
+                            accept={imageTypes.reduce((a, val) => `${a}, ${val}`)}
+                            style={{ display: "none" }}
+                            onChange={async (e) => {
+                                if (e.target.files!.length > 0) {
+                                    let file = e.target.files!.item(0)!;
+                                    await this.setImage(file);
                                 }
-                            }}
-                            onDragOver={e => {
-                                // prevents file from being opened in browser
-                                e.stopPropagation();
-                                e.preventDefault();
-                            }}
-                            onMouseMove={e => {
-                                let canvas = e.target as HTMLCanvasElement;
-                                var rect = canvas.getBoundingClientRect();
-                                let mouse = {
-                                    x: (e.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
-                                    y: (e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
-                                };
-                                mouse.x = Math.round(mouse.x);
-                                mouse.y = Math.round(mouse.y);
-                                // TODO: account for -1 value
+                            }} />
+                        <div
+                            className='overflow-auto'
+                            style={{
+                                textAlign: 'center',
+                                maxHeight: 1024,
+                                overflow: 'scroll'
+                            }}>
+                            <canvas id="myCanvas" width="200" height="100"
+                                onDrop={async (e) => {
+                                    // prevents file from being opened by browser
+                                    e.stopPropagation();
+                                    e.preventDefault();
 
-                                let context = canvas.getContext("2d", { willReadFrequently: true });
-                                let imgData = context!.getImageData(mouse.x, mouse.y, 1, 1)!;
+                                    this.setState({ dragging: false });
+                                    if (e.dataTransfer.files.length > 0) {
+                                        let firstFile = e.dataTransfer.files.item(0)!;
+                                        if (firstFile != null) {
+                                            await this.setImage(firstFile);
+                                        }
+                                    }
+                                }}
+                                onDragOver={e => {
+                                    // prevents file from being opened by browser
+                                    e.stopPropagation();
+                                    e.preventDefault();
+                                }}
+                                onDragEnter={() => this.setState({ dragging: true })}
+                                onDragLeave={() => this.setState({ dragging: false })}
+                                onMouseMove={e => {
+                                    let canvas = e.target as HTMLCanvasElement;
+                                    var rect = canvas.getBoundingClientRect();
+                                    let mouse = {
+                                        x: (e.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
+                                        y: (e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
+                                    };
+                                    mouse.x = Math.max(0, Math.round(mouse.x));
+                                    mouse.y = Math.max(0, Math.round(mouse.y));
 
-                                let color = new RGBAColor(imgData.data[0], imgData.data[1], imgData.data[2], imgData.data[3]);
-                                let sample = new PixelSample(mouse.x, mouse.y, color);
+                                    let context = canvas.getContext("2d", { willReadFrequently: true });
+                                    let imgData = context!.getImageData(mouse.x, mouse.y, 1, 1)!;
 
-                                this.setState({ currentSample: sample });
-                            }}
-                            onMouseUp={() => {
-                                let samples = this.state.pickedSamples;
-                                samples.push(this.state.currentSample);
-                                this.setState({ pickedSamples: samples })
-                            }}
-                            style={{ border: '1px solid #000000' }}>
-                        </canvas>
+                                    let color = new RGBAColor(imgData.data[0], imgData.data[1], imgData.data[2], imgData.data[3]);
+                                    let sample = new PixelSample(mouse.x, mouse.y, color);
+
+                                    this.setState({ currentSample: sample });
+                                }}
+                                onMouseUp={() => {
+                                    let samples = this.state.pickedSamples;
+                                    samples.push(this.state.currentSample);
+                                    this.setState({ pickedSamples: samples })
+                                }}
+                                style={{ border: this.state.dragging ? '2px dashed white' : '2px solid white' }}
+                            />
+                        </div>
                     </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <h3>Picked Samples</h3>
-                    </Col>
-                    <Col>
-                        {this.state.pickedSamples.length > 0 &&
-                            <Button variant='danger'
-                                onClick={() => this.setState({ pickedSamples: [] })}>Clear All</Button>
-                        }
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <ListGroup numbered variant='flush'>
-                            {this.state.pickedSamples.map((sample, idx) =>
-                                <ListGroup.Item key={idx} className="d-flex justify-content-between align-items-start">
-                                    <PixelSampleComponent sample={sample} />
-                                    <Badge
+                    <Col md={4}>
+                        <Card>
+                            <Card.Header className="d-flex justify-content-between">
+                                <Card.Title>Picked Samples</Card.Title>
+                                <ButtonGroup>
+                                    <Button variant='primary'
                                         onClick={() => {
-                                            let samples = this.state.pickedSamples;
-                                            samples.splice(idx, 1);
-                                            this.setState({ pickedSamples: samples });
-                                        }}
-                                        bg="danger"
-                                        pill>X</Badge>
-                                </ListGroup.Item>
-                            )}
-                        </ListGroup>
+                                            this.setState({ sortToggle: !this.state.sortToggle });
+                                        }}>
+                                        {!this.state.sortToggle && <Icon.ArrowDown size={20} />}
+                                        {this.state.sortToggle && <Icon.ArrowUp size={20} />}
+                                    </Button>
+                                    <Button variant='primary'
+                                        disabled={this.state.pickedSamples.length == 0}
+                                        onClick={() => this.exportPointsToJSON()}>
+                                        <Icon.Download style={{ marginRight: '0.5rem' }} size={20} />
+                                        JSON
+                                    </Button>
+                                    <Button variant='danger'
+                                        disabled={this.state.pickedSamples.length == 0}
+                                        onClick={() => this.setState({ pickedSamples: [] })}>
+                                        <Icon.Trash style={{ marginRight: '0.5rem' }} size={20} />
+                                        {this.state.pickedSamples.length}
+                                    </Button>
+                                </ButtonGroup>
+                            </Card.Header>
+                            <Card.Body>
+                                <ListGroup>
+                                    <ListGroup.Item active>
+                                        <PixelSampleComponent sample={this.state.currentSample} />
+                                    </ListGroup.Item>
+                                </ListGroup>
+                                <br />
+                                <ListGroup variant='flush'>
+                                    {(this.state.sortToggle ?
+                                        this.state.pickedSamples.slice(0).reverse() :
+                                        this.state.pickedSamples).map((sample, idx) =>
+                                            <ListGroup.Item key={idx} className="d-flex justify-content-between align-items-center">
+                                                <div className="ms-2 me-auto">
+                                                    <PixelSampleComponent sample={sample} />
+                                                </div>
+                                                <div className="ms-2 me-auto">
+                                                    <Button
+                                                        onClick={() => {
+                                                            let samples = this.state.pickedSamples;
+                                                            samples.splice(idx, 1);
+                                                            this.setState({ pickedSamples: samples });
+                                                        }}
+                                                        variant="danger">
+                                                        <Icon.XLg />
+                                                    </Button>
+                                                </div>
+                                            </ListGroup.Item>
+                                        )
+                                    }
+                                </ListGroup>
+                            </Card.Body>
+                        </Card>
                     </Col>
-                    <Col sm={5}></Col>
                 </Row>
             </Container>
         </div>
@@ -239,6 +318,13 @@ class App extends React.Component<any, AppState>  {
 
 }
 
-(document.getElementsByTagName("body")[0] as Element).innerHTML = "<div id='react-app'></div>";
+// insert <div> to render the React App to
+const body = (document.getElementsByTagName("body")[0] as HTMLBodyElement);
+body.innerHTML = "<div id='react-app'></div>";
+
 const reactAppElement = document.getElementById('react-app') as Element;
-createRoot(reactAppElement).render(<React.StrictMode><App /></React.StrictMode>);
+createRoot(reactAppElement).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,40 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
-module.exports = [{
-    entry: './App.tsx',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.tsx?$/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "@babel/preset-typescript",
-                            "@babel/preset-react"
-                        ]
-                    }
+
+module.exports = [
+    {
+        entry: './App.tsx',
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [MiniCssExtractPlugin.loader, "css-loader"],
                 },
-                exclude: /node_modules/,
-            },
+                {
+                    test: /\.tsx?$/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                "@babel/preset-typescript",
+                                "@babel/preset-react"
+                            ]
+                        }
+                    },
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: { extensions: ['.tsx', '.ts', '.js'] },
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: 'app.js',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({ title: "Pixel Picker" }),
+            new MiniCssExtractPlugin(),
         ],
     },
-    resolve: { extensions: ['.tsx', '.ts', '.js'] },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'app.js',
-    },
-    plugins: [new HtmlWebpackPlugin({title: "Pixel Picker"})],
-},
-
 ];
